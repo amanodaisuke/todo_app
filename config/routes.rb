b93ae1314bdfa022d9aad76a692cc4c70b31ce29
amanodaisuke <amanodaisuke34@gmail.com>
2023-07-23 00:36:18 +0000

    finish 2-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  resources :todos
-  resources :goals
-  devise_for :users
+   
+ resources :goals do
+   resources :todos
+     member do
+       get "soft"
+     end
+   end
+  end
+
+ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: "home#index"
-end
+
+ root to: "home#index"
+
